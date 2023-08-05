5fdd5ea936132c75382dc69fb6eb355ffadf6964
Bao Dang <danglequocbao001@gmail.com>
2023-08-06 00:05:28 +0700

    fix: comment

--- app/controllers/binh-luan.controller.js
+++ app/controllers/binh-luan.controller.js
@@ -3,9 +3,39 @@ const { JWT_PRIVATE_KEY } = require("../helper/constants");
 const jwt = require("jsonwebtoken");
 const db = require("../models");
 const BinhLuan = db.BinhLuan;
+const ChiTietDonDatHang = db.CTDonDatHang;
+const Op = db.Sequelize.Op;
 
-exports.findAll = (req, res) => {
+exports.findAll = async (req, res) => {
   const ma_mh = req.params.ma_mh;
+
+  let CTDDH = [];
+
+  if (req.headers && req.headers.authorization) {
+    const customerToken = req.headers.authorization;
+    const token = customerToken.split(" ");
+    const decoded = jwt.verify(token[0], JWT_PRIVATE_KEY);
+
+    const ma_kh = decoded.ma_kh;
+    const DDH = await db.DonDatHang.findAll({
+      where: { ma_kh: ma_kh },
+      raw: true,
+    });
+
+    const listIdDDH = DDH.map((item) => {
+      return item.ma_don_dat_hang;
+    });
+
+    CTDDH = await ChiTietDonDatHang.findAll({
+      where: {
+        ma_mh: ma_mh,
+        ma_don_dat_hang: {
+          [Op.in]: listIdDDH,
+        },
+      },
+      raw: true,
+    });
+  }
 
   BinhLuan.findAll({
     where: { ma_mh: ma_mh },
@@ -13,9 +43,10 @@ exports.findAll = (req, res) => {
   })
     .then(async (binhLuan) => {
       const result = await addCusToComment(binhLuan);
-      res
-        .status(200)
-        .send(result.sort((a, b) => b.ma_binh_luan - a.ma_binh_luan));
+      res.status(200).send({
+        data: result.sort((a, b) => b.ma_binh_luan - a.ma_binh_luan),
+        is_can_comment: CTDDH.length > 0 ? true : false,
+      });
     })
     .catch((err) => {
       res.status(500).send({
